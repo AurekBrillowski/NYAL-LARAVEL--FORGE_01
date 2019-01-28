f33ee016a0dabfb062f9c2e67b7c0a56ec64413a
AurekBrillowski <aurek@parkcitiesxc.org>
2019-01-28 12:55:41 -0600

    Home Page: 'NYAL Testing Server'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    NYAL Testing Server
                 </div>
 
                 <div class="links">
