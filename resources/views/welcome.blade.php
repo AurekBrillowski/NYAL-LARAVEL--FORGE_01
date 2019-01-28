433b40300e31e6fb386539da75303f678ac2df8f
AurekBrillowski <aurek@parkcitiesxc.org>
2019-01-28 12:54:12 -0600

    Home Page: 'NYAL Production Server'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    NYAL Production Server
                 </div>
 
                 <div class="links">
