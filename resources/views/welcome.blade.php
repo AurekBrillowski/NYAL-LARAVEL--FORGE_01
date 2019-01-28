8374a967ee7fb168657ae46c52835e1361b69176
AurekBrillowski <aurek@parkcitiesxc.org>
2019-01-28 12:52:09 -0600

    Initial Commit - DEVELOPMENT

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    NYAL Development Server
                 </div>
 
                 <div class="links">
