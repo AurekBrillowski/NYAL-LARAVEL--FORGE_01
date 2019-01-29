1737a65eb8bd573d31b99520b0128c1e052a82c9
AurekBrillowski <aurek@parkcitiesxc.org>
2019-01-28 20:48:44 -0600

    Remove 'Server' from 'NYAL Development' on home page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    NYAL Development Server
+                    NYAL - Development
                 </div>
 
                 <div class="links">
